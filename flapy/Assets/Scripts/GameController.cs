d4cb09dfbbaaf6d15901810bac77d5e717426a98
Thorien <amaderka@gmail.com>
2015-04-03 21:29:16 +0200

    Added new project Flapy
    updated Rules

---
+++ flapy/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float scenerySpeed = 10f;
+	public bool b_gameStarted = false;
+
+	private GameObject go_obstacleMover;
+	private GameObject go_Player;
+	private GameObject go_Scenery;
+
+
+	void Awake (){
+		go_obstacleMover = GameObject.Find("ObstacleMover");
+		go_Player = GameObject.Find("Player");
+		go_Scenery = GameObject.Find("Scenery");
+	}
+
+	void Update(){
+		if (b_gameStarted) {
+			if (go_Player != null){
+				go_Player.GetComponent<freezePlayer>().UnFreezPlayer();
+			}
+
+			go_obstacleMover.GetComponent<ObstacleMover>().b_moveObstacle = true;
+			go_Scenery.GetComponent<PlaySceneery>().playScenery = true;
+		}
+	}
+}
---
+++ flapy/Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMover : MonoBehaviour {
+
+	public float f_obstaclesSpeed = 4;
+	public float f_leftBoundary = -10;
+	public float f_rightBoundary = 10;
+	public float f_randomScaleMin = 0.5f;
+	public float f_randomScaleMax = 2f;
+	public GameObject[] GO_obstacleMesh;
+	public bool b_moveObstacle = false;
+
+	void Awake (){
+		GO_obstacleMesh = GameObject.FindGameObjectsWithTag("Obstacle");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (b_moveObstacle) {
+			transform.position = new Vector3 (transform.position.x - (f_obstaclesSpeed * Time.deltaTime),0f,0f);
+			if (transform.position.x <= f_leftBoundary) {
+				transform.position = new Vector3(f_rightBoundary,0f,0f);
+				f_obstaclesSpeed++;
+			}
+		}
+	
+	}
+}
